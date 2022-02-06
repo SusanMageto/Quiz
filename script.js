d5a353d3cde792aad4b2173968d9f66e6405c02c
SusanMageto <susan.mageto@student.moringaschool.com>
2022-02-07 00:00:34 +0300

    adds marks for each correct answer

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function lastButton() {
-  document.write ("Quiz Finished!");
+  var marks=0;
+  var correctAnswer1=document.questions.qn1.value;
+  var correctAnswer1=document.questions.qn1.value;
+  var correctAnswer1=document.questions.qn1.value;
+  var correctAnswer1=document.questions.qn1.value;
+  var correctAnswer1=document.questions.qn1.value;
+  var correctAnswer1=document.questions.qn1.value;
+  if (correctAnswer=="Object Oriented Language"){marks++};
+  document.write(marks);
 }
