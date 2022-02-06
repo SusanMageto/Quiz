1633a7305bee80bb68b9a42474550a733995ae20
SusanMageto <susan.mageto@student.moringaschool.com>
2022-02-07 00:31:20 +0300

    adds total marks

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function lastButton() {
   var marks=0;
   var correctAnswer1=document.questions.qn1.value;
-  var correctAnswer1=document.questions.qn1.value;
-  var correctAnswer1=document.questions.qn1.value;
-  var correctAnswer1=document.questions.qn1.value;
-  var correctAnswer1=document.questions.qn1.value;
-  var correctAnswer1=document.questions.qn1.value;
-  if (correctAnswer=="Object Oriented Language"){marks++};
-  document.write(marks);
+  var correctAnswer2=document.questions.qn2.value;
+  var correctAnswer3=document.questions.qn3.value;
+  var correctAnswer4=document.questions.qn4.value;
+  var correctAnswer5=document.questions.qn5.value;
+  var move=getElementById("move");
+  if (correctAnswer1=="Object Oriented Language"){marks++};
+  if (correctAnswer2=="Both A and B"){marks++};
+  if (correctAnswer3=="Both A and B"){marks++};
+  if (correctAnswer4=="Ignores the statements"){marks++};
+  if (correctAnswer5=="Const"){marks++};
+  questions.style.display="none";
+  move.textContent=marks;
 }
